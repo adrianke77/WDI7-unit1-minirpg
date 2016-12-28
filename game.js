183c88bcc68f94e3b3bc820eeb7c417ea5e1fda9
adrianke77 <adrianke77@gmail.com>
2016-12-28 22:50:16 +0800

    added player initialization

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// GAME SYSTEM CONSTRUCTOR
 function GameSys() {
   this.food = 0
   this.health = 0
@@ -5,26 +6,53 @@ function GameSys() {
   this.level = 0
   this.seedNumb
 }
-GameSys.prototype.initNumbSeedFromString = function( string ) {
+GameSys.prototype.initNumbSeedFromString = function(string) {
   // returns a number betwen 0 and 1 that will always generate for the string
-  var valuesArr = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ ".split( "" )
-  var stringArr = string.split( "" )
+  var valuesArr = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ ".split("")
+  var stringArr = string.split("")
   var seed = 13 // start value only
-  for ( var i = 0; i < string.length; i++ ) {
-    charValue = valuesArr.indexOf( stringArr[ i ] )
+  for (var i = 0; i < string.length; i++) {
+    charValue = valuesArr.indexOf(stringArr[i])
     seed += charValue
   }
-  var x = Math.sin( seed++ ) * 10000;
-  numbSeed = x - Math.floor( x );
+  var x = Math.sin(seed++) * 10000;
+  numbSeed = x - Math.floor(x);
+}
+GameSys.prototype.makeKeyListeners = function() {
+  $(document).keyup(function(event) {
+    switch (event.which) {
+      case 87: //w, NW direction
+        this.NW()
+        break
+      case 69: //e, NE direction
+        this.NE()
+        break
+      case 68: //d, E direction
+        this.E()
+        break
+      case 65: //a, W direction
+        this.W()
+        break
+      case 88: //x, SE direction
+        this.SE()
+        break
+      case 90: //z, SW direction
+        this.SW()
+        break
+    }
+  })
 }
 
 //testing stuff
-var worldBoard = new WorldBoard( 20, 20 );
+var worldBoard = new WorldBoard(20, 20);
 var gameSys = new GameSys()
+gameSys.initNumbSeedFromString("gargamel")
+console.log(numbSeed)
 worldBoard.makeBlankBoardHtml();
-gameSys.initNumbSeedFromString("Jeremiah" )
-console.log( numbSeed )
 worldBoard.randomizeTerrain()
 worldBoard.drawTerrain()
+worldBoard.initPlayer()
 worldBoard.randomizeCreatures()
+console.log(worldBoard.creatureLocs)
 worldBoard.drawCreatures()
+  // gameSys.makeKeyListeners()
--- worldBoard.js
+++ worldBoard.js
@@ -1,12 +1,12 @@
 const landTileSprites = {
-  plain: [ 0, 0 ],
-  swamp: [ -36, 0 ],
-  hill: [ -72, 0 ],
-  water: [ -18, -33 ],
-  mountain: [ -54, -33 ],
-  snow: [ 0, -66 ],
-  forest: [ -36, -66 ],
-  dune: [ -72, -66 ]
+  plain: [0, 0],
+  swamp: [-36, 0],
+  hill: [-72, 0],
+  water: [-18, -33],
+  mountain: [-54, -33],
+  snow: [0, -66],
+  forest: [-36, -66],
+  dune: [-72, -66]
 }
 
 const creatureTypes = {
@@ -14,155 +14,185 @@ const creatureTypes = {
   // 6: spriteX, 7: spriteY 8:rarity]
   //sprite X and Y are in 1-index
   //the higher the rarity the less common the monster
-  goblin: [ 3, "walk", 2, 1, 2, 2, 4, 2, 10 ],
-  dog: [ 2, "walk", 2, 2, 1, 2, 7, 3, 20 ],
-  eagle: [ 2, "fly", 1, 3, 1, 2, 6, 5, 30 ]
+  //treasureMax is a non-included max
+  //for drawing sprite
+  goblin: [3, "walk", 2, 1, 2, 4, 4, 2, 10],
+  dog: [2, "walk", 2, 2, 0, 3, 7, 3, 20],
+  eagle: [2, "fly", 1, 3, 0, 3, 6, 5, 30]
 
 }
 
 var numbSeed = 0
   // this number will change every time genNumbSeed is called
 
+
+//REPEATABLE RANDOM GENERATION
 function genNum() {
   //every time this function is called it will both 
   // replace global numbSeed value as well as
   // return the new numbSeed value
-  var x = Math.sin( numbSeed++ ) * 10000;
-  numbSeed = x - Math.floor( x );
+  var x = Math.sin(numbSeed++) * 10000;
+  numbSeed = x - Math.floor(x);
   return numbSeed
 }
 
-function genInt( max ) {
-  //uses genNum to return an integer between 0 and one less than limit
-  //
-  return Math.floor( genNum() * ( max ) );
+function genInt(max) {
+  //uses genNum to return an integer between 0 and max, 
+  //including 0 and excluding max
+  return Math.floor(genNum() * (max));
 }
 
-function Creature( type, statsArr ) {
+function genRange(min, max) {
+  //uses genNum to return an integer in range, 
+  //including min and excluding max
+}
+//PLAYER CONSTRUCTOR
+function Player(name) {
+  this.name = name
+  this.health = 25
+  this.speed = 1
+  this.strength = 2
+  this.gold = 10
+  this.movement = "walk"
+  this.type = "player"
+}
+
+//CREATURE CONSTRUCTOR
+function Creature(type, statsArr) {
   this.type = type;
-  this.health = statsArr[ 0 ];
-  this.movement = statsArr[ 1 ];
-  this.strength = statsArr[ 2 ];
-  this.speed = statsArr[ 3 ];
-  this.treasureMin = statsArr[ 4 ];
-  this.treasureMax = statsArr[ 5 ];
+  this.health = statsArr[0];
+  this.movement = statsArr[1];
+  this.strength = statsArr[2];
+  this.speed = statsArr[3];
+  this.treasureMin = statsArr[4];
+  this.treasureMax = statsArr[5];
 }
 
-function WorldBoard( width, height ) {
+//WORLDBOARD CONSTRUCTOR
+function WorldBoard(width, height) {
   this.height = height;
   this.width = width;
   this.landTiles = []; // stores terrain as strings
   this.creatureLocs = []; //stores creatures as Creature instances
   this.objectLocs = [];
-  for ( var i = 0; i < this.height; i++ ) {
-    this.landTiles[ i ] = [];
-    this.creatureLocs[ i ] = [];
-    this.objectLocs[ i ] = []
-    for ( var j = 0; j < this.width; j++ ) {
-      this.landTiles[ i ][ j ] = null;
-      this.creatureLocs[ i ][ j ] = null;
-      this.objectLocs[ i ][ j ] = null;
+  for (var i = 0; i < this.height; i++) {
+    this.landTiles[i] = [];
+    this.creatureLocs[i] = [];
+    this.objectLocs[i] = []
+    for (var j = 0; j < this.width; j++) {
+      this.landTiles[i][j] = null;
+      this.creatureLocs[i][j] = null;
+      this.objectLocs[i][j] = null;
     }
   }
 }
 WorldBoard.prototype.makeBlankBoardHtml = function() {
-  this.landTiles.forEach( function( row, rowidx ) {
-    row.forEach( function( tile, colidx ) {
-      var landCell = $( "<div></div>" );
-      var creatureCell = $( "<div></div>" );
+  this.landTiles.forEach(function(row, rowidx) {
+    row.forEach(function(tile, colidx) {
+      var landCell = $("<div></div>");
+      var creatureCell = $("<div></div>");
       landCell.addClass(
-        "landbox landR" + rowidx + "C" + colidx + " background" );
+        "landbox landR" + rowidx + "C" + colidx + " background");
       creatureCell.addClass(
-        "creatbox creatR" + rowidx + "C" + colidx );
+        "creatbox creatR" + rowidx + "C" + colidx);
       var rowshift =
         rowidx % 2 === 0 ? 15 : 0;
       var topPos = 24 * rowidx;
       var leftPos = 30 * colidx + rowshift;
-      landCell.css( {
+      landCell.css({
         "top": topPos,
         "left": leftPos
-      } );
-      creatureCell.css( {
+      });
+      creatureCell.css({
         "top": topPos,
         "left": leftPos
-      } );
-      $( ".gameBoard" ).append( landCell ).append( creatureCell );
-    } );
-  } );
+      });
+      $(".gameBoard").append(landCell).append(creatureCell);
+    });
+  });
 }
 WorldBoard.prototype.drawTerrain = function() {
-  this.landTiles.forEach( function( row, rowidx ) {
-    row.forEach( function( tile, colidx ) {
-      var xpos = landTileSprites[ tile ][ 0 ];
-      var ypos = landTileSprites[ tile ][ 1 ];
+  this.landTiles.forEach(function(row, rowidx) {
+    row.forEach(function(tile, colidx) {
+      var xpos = landTileSprites[tile][0];
+      var ypos = landTileSprites[tile][1];
       var posString = xpos + "px " + ypos + "px";
-      $( ".landR" + rowidx + "C" + colidx ).css( "background-position", posString );
-    } );
-  } );
+      $(".landR" + rowidx + "C" + colidx).css("background-position", posString);
+    });
+  });
 }
 WorldBoard.prototype.randomizeTerrain = function() {
   var x = this.width;
   var y = this.height;
   var tilesNumber = x * y;
-  this.landTiles.forEach( function( row, rowidx, arr ) {
-    row.forEach( function( tile, colidx, row ) {
-      arr[ rowidx ][ colidx ] = "plain" //initialize all as plain
-    } )
-  } )
-  var swampTiles = Math.floor( tilesNumber / 4 );
-  var forestTiles = Math.floor( tilesNumber / 3 );
-  var mountainTiles = Math.floor( tilesNumber / 5 );
-  var waterTiles = Math.floor( tilesNumber / 2 );
-  for ( var i = 0; i < waterTiles; i++ )
-    this.landTiles[ genInt( y ) ][ genInt( x ) ] = "water";
-  for ( var i = 0; i < swampTiles; i++ )
-    this.landTiles[ genInt( y ) ][ genInt( x ) ] = "swamp";
-  for ( var i = 0; i < forestTiles; i++ )
-    this.landTiles[ genInt( y ) ][ genInt( x ) ] = "forest";
-  for ( var i = 0; i < mountainTiles; i++ )
-    this.landTiles[ genInt( y ) ][ genInt( x ) ] = "mountain";
+  this.landTiles.forEach(function(row, rowidx, arr) {
+    row.forEach(function(tile, colidx, row) {
+      arr[rowidx][colidx] = "plain" //initialize all as plain
+    })
+  })
+  var swampTiles = Math.floor(tilesNumber / 4);
+  var forestTiles = Math.floor(tilesNumber / 3);
+  var mountainTiles = Math.floor(tilesNumber / 5);
+  var waterTiles = Math.floor(tilesNumber / 2);
+  for (var i = 0; i < waterTiles; i++)
+    this.landTiles[genInt(y)][genInt(x)] = "water";
+  for (var i = 0; i < swampTiles; i++)
+    this.landTiles[genInt(y)][genInt(x)] = "swamp";
+  for (var i = 0; i < forestTiles; i++)
+    this.landTiles[genInt(y)][genInt(x)] = "forest";
+  for (var i = 0; i < mountainTiles; i++)
+    this.landTiles[genInt(y)][genInt(x)] = "mountain";
 }
 WorldBoard.prototype.randomizeCreatures = function() {
   //name:[health,movement,strength,speed,treasureMin,treasureMax,spriteX,spriteY]
   //sprite X and Y are in 1-index
   self = this
-  for ( creatureType in creatureTypes ) {
+  for (creatureType in creatureTypes) {
     var y = this.height;
     var x = this.width;
     var tilesNumber = x * y;
-    noOfMonster = Math.floor( tilesNumber / creatureTypes[ creatureType ][ 8 ] );
-    for ( var i = 0; i < noOfMonster; i++ ) {
-      this.makeACreature(self,x,y)
+    noOfMonster = Math.floor(tilesNumber / creatureTypes[creatureType][8]);
+    for (var i = 0; i < noOfMonster; i++) {
+      this.makeACreature(self, x, y)
     }
   }
 }
-WorldBoard.prototype.makeACreature = function(self,x,y) {
-  xpos = genInt( x )
-  ypos = genInt( y )
-  var terrainHere = self.landTiles[ ypos ][ xpos ];
-  var movement = creatureTypes[ creatureType ][ 1 ]
-  if ( !( movement === "walk" && terrainHere === "water" ) )
-    if ( !( movement === "fly" && terrainHere === "forest" ) ) {
-      this.creatureLocs[ ypos ][ xpos ] =
-        new Creature( creatureType, creatureTypes[ creatureType ] );
-      return
-    }
+WorldBoard.prototype.makeACreature = function(self, x, y) {
+  xpos = genInt(x)
+  ypos = genInt(y)
+  var terrainHere = self.landTiles[ypos][xpos];
+  var movement = creatureTypes[creatureType][1]
+  if (!(movement === "walk" && terrainHere === "water"))
+    if (!(movement === "fly" && terrainHere === "forest"))
+      if (!(xpos === 1 && ypos === 1)) {
+        this.creatureLocs[ypos][xpos] =
+          new Creature(creatureType, creatureTypes[creatureType]);
+        return
+      }
   self = this; // if fail make the same creature again, at new random point
-  self.makeACreature(self,x,y)
+  self.makeACreature(self, x, y)
 }
 WorldBoard.prototype.drawCreatures = function() {
-  this.creatureLocs.forEach( function( row, rowidx ) {
-    row.forEach( function( creature, colidx ) {
-      if ( creature ) {
-        var xpos = ( creatureTypes[ creature.type ][ 6 ] - 1 ) * -20
-        var ypos = ( creatureTypes[ creature.type ][ 7 ] - 1 ) * -20.5
+  this.creatureLocs.forEach(function(row, rowidx) {
+    row.forEach(function(creature, colidx) {
+      if (creature) {
+        if (creature.type === "player") {
+          var xpos = (6 - 1) * -20
+          var ypos = (2 - 1) * -20.5
+        } else {
+          var xpos = (creatureTypes[creature.type][6] - 1) * -20
+          var ypos = (creatureTypes[creature.type][7] - 1) * -20.5
+        }
         var posString = xpos + "px " + ypos + "px";
         // make string to shift spritesheet to
-        var terrainHere = self.landTiles[ rowidx ][ colidx ];
-        $( ".creatR" + rowidx + "C" + colidx )
-          .addClass( "creature" )
-          .css( "background-position", posString );
+        var terrainHere = self.landTiles[rowidx][colidx];
+        $(".creatR" + rowidx + "C" + colidx)
+          .addClass("creature")
+          .css("background-position", posString);
       }
-    } )
-  } )
+    })
+  })
+}
+WorldBoard.prototype.initPlayer = function(playerName) {
+  this.creatureLocs[1][1] = new Player(playerName)
 }
